b2adcc4b3d3ff8ca131ec678adf766ab988115a6
Adrià Matas Arqueros <adriamatas@ceroca.cat>
2024-09-23 20:24:48 +0200

    limits pantalla

--- Assets/scripts/NauJugador.cs
+++ Assets/scripts/NauJugador.cs
@@ -4,10 +4,18 @@ public class Naujugador : MonoBehaviour
 {
     private float _vel;
 
+    private Vector2 minpantalla, maxPantalla;
+
+    public object ViewsportToWorldPoint { get; private set; }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _vel = 8f;
+
+        minPantalla = Camera.main.ViewsportToWorldPoint(new Vector2(0, 0));
+        maxPantalla = Camera.main.ViewsportToWorldPoint(new Vector2(1, 1));
+
     }
 
     // Update is called once per frame
@@ -21,6 +29,10 @@ public class Naujugador : MonoBehaviour
         Vector2 novaPos = transform.position; // pos actual de la nau 
         novaPos = novaPos + direccioindicada * _vel * Time.deltaTime;
         // Debug.Log(Time.deltaTime);
+
+
+        novaPos.x = Mathf.Clamp (novaPos.x, minpantalla.x, maxPantalla.x);
+        novaPos.y = Mathf.Clamp(novaPos.y, minpantalla.y, maxPantalla.y);
 
         transform.position = novaPos;
 
